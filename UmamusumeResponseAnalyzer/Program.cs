1404ef74db1dc0fb43d67d1c0e6762cc04c61d92
EtherealAO <admin@nijigen.cc>
2022-02-04 20:10:09 +0800

    增加简单TUI，增强表现效果

--- UmamusumeResponseAnalyzer/Program.cs
+++ UmamusumeResponseAnalyzer/Program.cs
@@ -1,13 +1,17 @@
 ﻿
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Spectre.Console;
 using System.Diagnostics;
+using System.Text;
+using System;
+using System.Text.RegularExpressions;
 
 namespace UmamusumeResponseAnalyzer
 {
     public static class UmamusumeResponseAnalyzer
     {
-        public static void Main()
+        public static async Task Main()
         {
 #if DEBUG
             var msgpackPath = @"response/637795073363227948.bin";
@@ -17,9 +21,103 @@ namespace UmamusumeResponseAnalyzer
             File.WriteAllText(jsonPath, JObject.Parse(json).ToString());
             File.WriteAllText(jsonPath + ".msgpack.json", JsonConvert.SerializeObject(Server.TryDeserialize<Gallop.SingleModeCheckEventResponse>(msgpackBytes), Formatting.Indented));
 #endif
+            Console.BufferWidth = 160;
+            Console.SetWindowSize(Console.BufferWidth, Console.WindowHeight + 3);
+            Console.OutputEncoding = Encoding.UTF8;
+            var prompt = string.Empty;
+            do
+            {
+                prompt = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title("Launch Menu")
+                    .PageSize(10)
+                    .AddChoices(new[]
+                            {
+                            "Start!",
+                            "Update events.json",
+                            "Kill process who occupied 4693 ports"
+                            }
+                    ));
+                switch (prompt)
+                {
+                    case "Update events.json":
+                        await AnsiConsole.Progress()
+                            .StartAsync(async ctx =>
+                            {
+                                var client = new HttpClient();
+                                var task = ctx.AddTask("Downloading events.json from github", false);
+                                using var response = await client.GetAsync("https://raw.githubusercontent.com/EtherealAO/UmamusumeResponseAnalyzer/master/events.json", HttpCompletionOption.ResponseContentRead);
+                                response.EnsureSuccessStatusCode();
+                                task.MaxValue(response.Content.Headers.ContentLength ?? 0);
+                                task.StartTask();
+                                using var contentStream = await response.Content.ReadAsStreamAsync();
+                                using var fileStream = new FileStream("events.json", FileMode.Create, FileAccess.Write, FileShare.None, 8192, true);
+                                var buffer = new byte[8192];
+                                while (true)
+                                {
+                                    var read = await contentStream.ReadAsync(buffer);
+                                    if (read == 0)
+                                        break;
+                                    task.Increment(read);
+                                    await fileStream.WriteAsync(buffer.AsMemory(0, read));
+                                }
+                            });
+                        Console.Clear();
+                        AnsiConsole.MarkupLine($"Download completed!");
+                        Console.WriteLine("Press any key to return main menu...");
+                        Console.ReadKey();
+                        break;
+                    case "Kill process who occupied 4693 ports":
+                        using (var Proc = new Process())
+                        {
+
+                            var StartInfo = new ProcessStartInfo();
+                            StartInfo.FileName = "netstat.exe";
+                            StartInfo.Arguments = "-a -n -o";
+                            StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            StartInfo.UseShellExecute = false;
+                            StartInfo.RedirectStandardInput = true;
+                            StartInfo.RedirectStandardOutput = true;
+                            StartInfo.RedirectStandardError = true;
+                            Proc.StartInfo = StartInfo;
+                            Proc.Start();
+                            var NetStatRows = Regex.Split(Proc.StandardOutput.ReadToEnd() + Proc.StandardError.ReadToEnd(), "\r\n");
+                            foreach (string NetStatRow in NetStatRows)
+                            {
+                                string[] Tokens = Regex.Split(NetStatRow, "\\s+");
+                                if (Tokens.Length > 4 && (Tokens[1].Equals("UDP") || Tokens[1].Equals("TCP")))
+                                {
+                                    string IpAddress = Regex.Replace(Tokens[2], @"\[(.*?)\]", "1.1.1.1");
+                                    var port = Convert.ToInt32(IpAddress.Split(':')[1]);
+                                    if (port != 4693) continue;
+                                    var pid = Tokens[1] == "UDP" ? Convert.ToInt16(Tokens[4]) : Convert.ToInt16(Tokens[5]);
+                                    var name = Tokens[1] == "UDP" ? Process.GetProcessById(pid).ProcessName : Process.GetProcessById(pid).ProcessName;
+
+                                    var decision = AnsiConsole.Confirm($"Confirm to kill ProcessId:{pid} Name:{name}?");
+                                    if (decision)
+                                    {
+                                        if (name == "System")
+                                        {
+                                            Console.WriteLine("YOU CAN'T KILL SYSTEM! Please follow FAQ to solve conflict problem.");
+                                            break;
+                                        }
+                                        Process.GetProcessById(pid).Kill();
+                                    }
+                                    else
+                                    {
+                                        Environment.Exit(1);
+                                    }
+                                }
+                            }
+                        }
+                        Console.WriteLine("Program using 4693 port not found!");
+                        Console.WriteLine("Press any key to return main menu...");
+                        Console.ReadLine();
+                        break;
+                }
+                Console.Clear();
+            } while (prompt != "Start!");
             Database.Initialize();
             Server.Start();
-            Console.WriteLine("已启动.");
             while (true)
             {
                 Console.ReadLine();
--- UmamusumeResponseAnalyzer/Server.cs
+++ UmamusumeResponseAnalyzer/Server.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using Spectre.Console;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -80,15 +81,16 @@ namespace UmamusumeResponseAnalyzer
                 foreach (var i in @event.data.unchecked_event_array)
                 {
                     if (i.event_contents_info?.choice_array.Length == 0) continue;
-                    Console.WriteLine($"————————{Database.Events[i.story_id].Name} | {Database.Events[i.story_id].TriggerName}————————");
+                    var mainTree = new Tree(Database.Events[i.story_id].TriggerName.EscapeMarkup());
+                    var eventTree = new Tree(Database.Events[i.story_id].Name.EscapeMarkup());
                     for (var j = 0; j < i.event_contents_info.choice_array.Length; ++j)
                     {
-                        Console.WriteLine($"{Database.Events[i.story_id].Choices[j].Option} @ {i.event_contents_info.choice_array[j].select_index}");
-                        Console.WriteLine($"{Database.Events[i.story_id].Choices[j].Effect}");
-                        if ((j + 1) != i.event_contents_info.choice_array.Length)
-                            Console.WriteLine("～～～～～～～～～～～～～～～～");
+                        var tree = new Tree($"{Database.Events[i.story_id].Choices[j].Option} @ {i.event_contents_info.choice_array[j].select_index}".EscapeMarkup());
+                        tree.AddNode($"{Database.Events[i.story_id].Choices[j].Effect}".EscapeMarkup());
+                        eventTree.AddNode(tree);
                     }
-                    Console.WriteLine("—————————————————————————————————");
+                    mainTree.AddNode(eventTree);
+                    AnsiConsole.Write(mainTree);
                 }
             }
         }
@@ -99,17 +101,19 @@ namespace UmamusumeResponseAnalyzer
             if (@event != default && data.trained_chara_array.Length > 0 && data.trained_chara_favorite_array.Length > 0)
             {
                 var name = JObject.Parse("{\"100101\":\"[スペシャルドリーマー]スペシャルウィーク\",\"100102\":\"[ほっぴん♪ビタミンハート]スペシャルウィーク\",\"100201\":\"[サイレントイノセンス]サイレンススズカ\",\"100301\":\"[トップ・オブ・ジョイフル]トウカイテイオー\",\"100302\":\"[ビヨンド・ザ・ホライズン]トウカイテイオー\",\"100401\":\"[フォーミュラオブルージュ]マルゼンスキー\",\"100402\":\"[ぶっとび☆さまーナイト]マルゼンスキー\",\"100501\":\"[シューティンスタァ・ルヴュ]フジキセキ\",\"100601\":\"[スターライトビート]オグリキャップ\",\"100602\":\"[キセキの白星]オグリキャップ\",\"100701\":\"[レッドストライフ]ゴールドシップ\",\"100801\":\"[ワイルドトップギア]ウオッカ\",\"100901\":\"[トップ・オブ・ブルー]ダイワスカーレット\",\"101001\":\"[ワイルド・フロンティア]タイキシャトル\",\"101101\":\"[岩穿つ青]グラスワンダー\",\"101102\":\"[セイントジェード・ヒーラー]グラスワンダー\",\"101201\":\"[アマゾネス・ラピス]ヒシアマゾン\",\"101301\":\"[エレガンス・ライン]メジロマックイーン\",\"101302\":\"[エンド・オブ・スカイ]メジロマックイーン\",\"101401\":\"[エル☆Número 1]エルコンドルパサー\",\"101402\":\"[ククルカン・モンク]エルコンドルパサー\",\"101501\":\"[オー・ソレ・スーオ！]テイエムオペラオー\",\"101502\":\"[初晴・青き絢爛]テイエムオペラオー\",\"101601\":\"[Maverick]ナリタブライアン\",\"101701\":\"[ロード・オブ・エンペラー]シンボリルドルフ\",\"101702\":\"[皓月の弓取り]シンボリルドルフ\",\"101801\":\"[エンプレスロード]エアグルーヴ\",\"101802\":\"[クエルクス・キウィーリス]エアグルーヴ\",\"101901\":\"[超特急！フルカラー特殊PP]アグネスデジタル\",\"102001\":\"[あおぐもサミング]セイウンスカイ\",\"102101\":\"[疾風迅雷]タマモクロス\",\"102201\":\"[Noble Seamair]ファインモーション\",\"102301\":\"[pf.Victory formula...]ビワハヤヒデ\",\"102302\":\"[ノエルージュ・キャロル]ビワハヤヒデ\",\"102401\":\"[すくらんぶる☆ゾーン]マヤノトップガン\",\"102402\":\"[サンライト・ブーケ]マヤノトップガン\",\"102501\":\"[Creeping Black]マンハッタンカフェ\",\"102601\":\"[MB-19890425]ミホノブルボン\",\"102602\":\"[CODE：グラサージュ]ミホノブルボン\",\"102701\":\"[ストレート・ライン]メジロライアン\",\"102801\":\"[ボーノ☆アラモーダ]ヒシアケボノ\",\"103001\":\"[ローゼスドリーム]ライスシャワー\",\"103002\":\"[Make up Vampire!]ライスシャワー\",\"103201\":\"[tach-nology]アグネスタキオン\",\"103501\":\"[Go To Winning!]ウイニングチケット\",\"103701\":\"[Meisterschaft]エイシンフラッシュ\",\"103702\":\"[コレクト・ショコラティエ]エイシンフラッシュ\",\"103801\":\"[フィーユ・エクレール]カレンチャン\",\"103901\":\"[プリンセス・オブ・ピンク]カワカミプリンセス\",\"104001\":\"[オーセンティック/1928]ゴールドシチー\",\"104002\":\"[秋桜ダンツァトリーチェ]ゴールドシチー\",\"104101\":\"[サクラ、すすめ！]サクラバクシンオー\",\"104501\":\"[マーマリングストリーム]スーパークリーク\",\"104502\":\"[シフォンリボンマミー]スーパークリーク\",\"104601\":\"[あぶそりゅーと☆LOVE]スマートファルコン\",\"104801\":\"[ポップス☆ジョーカー]トーセンジョーダン\",\"105001\":\"[Nevertheless]ナリタタイシン\",\"105201\":\"[うららん一等賞♪]ハルウララ\",\"105202\":\"[初うらら♪さくさくら]ハルウララ\",\"105601\":\"[運気上昇☆幸福万来]マチカネフクキタル\",\"105602\":\"[吉兆・初あらし]マチカネフクキタル\",\"105801\":\"[ブルー/レイジング]メイショウドトウ\",\"105901\":\"[ツイステッド・ライン]メジロドーベル\",\"106001\":\"[ポインセチア・リボン]ナイスネイチャ\",\"106101\":\"[キング・オブ・エメラルド]キングヘイロー\",\"106901\":\"[日下開山・花あかり]サクラチヨノオー\"}").ToObject<Dictionary<int, string>>();
-                { //自己的马
-                    var fav_ids = data.trained_chara_favorite_array.Select(x => x.trained_chara_id).ToList();
-                    var chara = data.trained_chara_array.Where(x => fav_ids.Contains(x.trained_chara_id));
-                    var win_saddle_result = new List<(string Name, int WinSaddle, int Score)>();
-                    foreach (var i in chara)
-                        win_saddle_result.Add((name[i.card_id], i.win_saddle_id_array.Intersect(i.succession_chara_array[0].win_saddle_id_array).Count()
-                        + i.win_saddle_id_array.Intersect(i.succession_chara_array[1].win_saddle_id_array).Count(), i.rank_score));
-                    win_saddle_result.Sort((a, b) => b.WinSaddle.CompareTo(a.WinSaddle));
-                    foreach (var (Name, WinSaddle, Score) in win_saddle_result)
-                        Console.WriteLine($"{Name}\t\t{WinSaddle}\t\t{Score}");
-                }
+                var fav_ids = data.trained_chara_favorite_array.Select(x => x.trained_chara_id).ToList();
+                var chara = data.trained_chara_array.Where(x => fav_ids.Contains(x.trained_chara_id));
+                var win_saddle_result = new List<(string Name, int WinSaddle, int Score)>();
+                foreach (var i in chara)
+                    win_saddle_result.Add((name[i.card_id], i.win_saddle_id_array.Intersect(i.succession_chara_array[0].win_saddle_id_array).Count()
+                    + i.win_saddle_id_array.Intersect(i.succession_chara_array[1].win_saddle_id_array).Count(), i.rank_score));
+                win_saddle_result.Sort((a, b) => b.WinSaddle.CompareTo(a.WinSaddle));
+                var table = new Table();
+                table.Border = TableBorder.Ascii;
+                table.AddColumns("种马名", "胜鞍加成", "分数");
+                foreach (var (Name, WinSaddle, Score) in win_saddle_result)
+                    table.AddRow(Name.EscapeMarkup(), WinSaddle.ToString(), Score.ToString());
+                AnsiConsole.Write(table);
             }
         }
         static void ParseFriendSearchResponse(byte[] buffer)
@@ -119,20 +123,23 @@ namespace UmamusumeResponseAnalyzer
             if (@event != default)
             {
                 var name = JObject.Parse("{\"100101\":\"[スペシャルドリーマー]スペシャルウィーク\",\"100102\":\"[ほっぴん♪ビタミンハート]スペシャルウィーク\",\"100201\":\"[サイレントイノセンス]サイレンススズカ\",\"100301\":\"[トップ・オブ・ジョイフル]トウカイテイオー\",\"100302\":\"[ビヨンド・ザ・ホライズン]トウカイテイオー\",\"100401\":\"[フォーミュラオブルージュ]マルゼンスキー\",\"100402\":\"[ぶっとび☆さまーナイト]マルゼンスキー\",\"100501\":\"[シューティンスタァ・ルヴュ]フジキセキ\",\"100601\":\"[スターライトビート]オグリキャップ\",\"100602\":\"[キセキの白星]オグリキャップ\",\"100701\":\"[レッドストライフ]ゴールドシップ\",\"100801\":\"[ワイルドトップギア]ウオッカ\",\"100901\":\"[トップ・オブ・ブルー]ダイワスカーレット\",\"101001\":\"[ワイルド・フロンティア]タイキシャトル\",\"101101\":\"[岩穿つ青]グラスワンダー\",\"101102\":\"[セイントジェード・ヒーラー]グラスワンダー\",\"101201\":\"[アマゾネス・ラピス]ヒシアマゾン\",\"101301\":\"[エレガンス・ライン]メジロマックイーン\",\"101302\":\"[エンド・オブ・スカイ]メジロマックイーン\",\"101401\":\"[エル☆Número 1]エルコンドルパサー\",\"101402\":\"[ククルカン・モンク]エルコンドルパサー\",\"101501\":\"[オー・ソレ・スーオ！]テイエムオペラオー\",\"101502\":\"[初晴・青き絢爛]テイエムオペラオー\",\"101601\":\"[Maverick]ナリタブライアン\",\"101701\":\"[ロード・オブ・エンペラー]シンボリルドルフ\",\"101702\":\"[皓月の弓取り]シンボリルドルフ\",\"101801\":\"[エンプレスロード]エアグルーヴ\",\"101802\":\"[クエルクス・キウィーリス]エアグルーヴ\",\"101901\":\"[超特急！フルカラー特殊PP]アグネスデジタル\",\"102001\":\"[あおぐもサミング]セイウンスカイ\",\"102101\":\"[疾風迅雷]タマモクロス\",\"102201\":\"[Noble Seamair]ファインモーション\",\"102301\":\"[pf.Victory formula...]ビワハヤヒデ\",\"102302\":\"[ノエルージュ・キャロル]ビワハヤヒデ\",\"102401\":\"[すくらんぶる☆ゾーン]マヤノトップガン\",\"102402\":\"[サンライト・ブーケ]マヤノトップガン\",\"102501\":\"[Creeping Black]マンハッタンカフェ\",\"102601\":\"[MB-19890425]ミホノブルボン\",\"102602\":\"[CODE：グラサージュ]ミホノブルボン\",\"102701\":\"[ストレート・ライン]メジロライアン\",\"102801\":\"[ボーノ☆アラモーダ]ヒシアケボノ\",\"103001\":\"[ローゼスドリーム]ライスシャワー\",\"103002\":\"[Make up Vampire!]ライスシャワー\",\"103201\":\"[tach-nology]アグネスタキオン\",\"103501\":\"[Go To Winning!]ウイニングチケット\",\"103701\":\"[Meisterschaft]エイシンフラッシュ\",\"103702\":\"[コレクト・ショコラティエ]エイシンフラッシュ\",\"103801\":\"[フィーユ・エクレール]カレンチャン\",\"103901\":\"[プリンセス・オブ・ピンク]カワカミプリンセス\",\"104001\":\"[オーセンティック/1928]ゴールドシチー\",\"104002\":\"[秋桜ダンツァトリーチェ]ゴールドシチー\",\"104101\":\"[サクラ、すすめ！]サクラバクシンオー\",\"104501\":\"[マーマリングストリーム]スーパークリーク\",\"104502\":\"[シフォンリボンマミー]スーパークリーク\",\"104601\":\"[あぶそりゅーと☆LOVE]スマートファルコン\",\"104801\":\"[ポップス☆ジョーカー]トーセンジョーダン\",\"105001\":\"[Nevertheless]ナリタタイシン\",\"105201\":\"[うららん一等賞♪]ハルウララ\",\"105202\":\"[初うらら♪さくさくら]ハルウララ\",\"105601\":\"[運気上昇☆幸福万来]マチカネフクキタル\",\"105602\":\"[吉兆・初あらし]マチカネフクキタル\",\"105801\":\"[ブルー/レイジング]メイショウドトウ\",\"105901\":\"[ツイステッド・ライン]メジロドーベル\",\"106001\":\"[ポインセチア・リボン]ナイスネイチャ\",\"106101\":\"[キング・オブ・エメラルド]キングヘイロー\",\"106901\":\"[日下開山・花あかり]サクラチヨノオー\"}").ToObject<Dictionary<int, string>>();
-                //var i = ParseWinSaddle(jo["data"]["practice_partner_info"]);
                 var i = data.practice_partner_info;
                 var (Name, WinSaddle, Score) = (name[i.card_id], i.win_saddle_id_array.Intersect(i.succession_chara_array[0].win_saddle_id_array).Count()
                         + i.win_saddle_id_array.Intersect(i.succession_chara_array[1].win_saddle_id_array).Count(), i.rank_score);
-                Console.WriteLine("—————————————————————————————————");
-                Console.WriteLine($"好友：{data.user_info_summary.name}\t\tFollower数：{data.follower_num}");
-                Console.WriteLine($"种马：{Name}\t\t{WinSaddle}\t\t{Score}");
-                Console.WriteLine("—————————————————————————————————");
+                AnsiConsole.Write(new Rule());
+                AnsiConsole.WriteLine($"好友：{data.user_info_summary.name}\t\tFollower数：{data.follower_num}");
+                AnsiConsole.WriteLine($"种马：{Name}\t\t{WinSaddle}\t\t{Score}");
+                AnsiConsole.Write(new Rule());
             }
         }
         static void ParseTeamStadiumOpponentListResponse(byte[] buffer)
         {
             var @event = TryDeserialize<Gallop.TeamStadiumOpponentListResponse>(buffer);
             var data = @event.data;
+            var container = new Table();
+            container.Border = TableBorder.Double;
+            container.AddColumn(new TableColumn(string.Empty).NoWrap());
+            container.HideHeaders();
             foreach (var i in data.opponent_info_array.OrderByDescending(x => x.strength))
             {
                 var Type = i.strength switch
@@ -141,25 +148,88 @@ namespace UmamusumeResponseAnalyzer
                     2 => "中",
                     3 => "下"
                 };
-                Console.WriteLine($"————————————————{Type}————————————————");
-                var dic = new Dictionary<int, string>();
-                foreach (var j in i.trained_chara_array)
-                    dic.Add(j.trained_chara_id, $"\t短{GetProper(j.proper_distance_short)}\t英{GetProper(j.proper_distance_mile)}\t中{GetProper(j.proper_distance_middle)}\t长{GetProper(j.proper_distance_long)}\t泥{GetProper(j.proper_ground_dirt)}{Environment.NewLine}\t速{j.speed}\t耐{j.stamina}\t力{j.power}\t根{j.guts}\t智{j.wiz}");
-                foreach (var j in i.team_data_array.GroupBy(x => x.distance_type))
+                var table = new Table();
+                table.Title(Type);
+                table.AddColumns(Enumerable.Repeat(new TableColumn("　　　").NoWrap(), 17).ToArray());
+                table.HideHeaders();
+                var teamData = i.team_data_array.GroupBy(x => x.distance_type).ToDictionary(x => x.Key, x => x.ToList());
+                var properTypeLine = new List<string> { string.Empty };
+                var properValueLine = new List<string> { "适性" };
+                var speedLine = new List<string> { "速度" };
+                var staminaLine = new List<string> { "耐力" };
+                var powerLine = new List<string> { "力量" };
+                var gutsLine = new List<string> { "根性" };
+                var wizLine = new List<string> { "智力" };
+                foreach (var j in teamData)
                 {
-                    var distance = j.Key switch
+                    foreach (var k in j.Value)
                     {
-                        1 => "短",
-                        2 => "英",
-                        3 => "中",
-                        4 => "长",
-                        5 => "泥"
-                    };
-                    Console.WriteLine($"{distance}: ");
-                    foreach (var k in j)
-                        Console.WriteLine(dic[k.trained_chara_id]);
+                        var trainedChara = i.trained_chara_array.First(x => x.trained_chara_id == k.trained_chara_id);
+                        var properType = string.Empty;
+                        var properValue = string.Empty;
+                        properType += (k.distance_type switch
+                        {
+                            5 => "泥",
+                            _ => "芝"
+                        });
+                        properValue += (k.distance_type switch
+                        {
+                            5 => GetProper(trainedChara.proper_ground_dirt),
+                            _ => GetProper(trainedChara.proper_ground_turf)
+                        });
+                        properValue += ' ';
+                        properType += (k.distance_type switch
+                        {
+                            1 => "短",
+                            2 => "英",
+                            3 => "中",
+                            4 => "长",
+                            5 => "英"
+                        });
+                        properValue += (k.distance_type switch
+                        {
+                            1 => GetProper(trainedChara.proper_distance_short),
+                            2 => GetProper(trainedChara.proper_distance_mile),
+                            3 => GetProper(trainedChara.proper_distance_middle),
+                            4 => GetProper(trainedChara.proper_distance_long),
+                            5 => GetProper(trainedChara.proper_distance_mile)
+                        });
+                        properValue += ' ';
+                        properType += (k.running_style switch
+                        {
+                            1 => "逃",
+                            2 => "先",
+                            3 => "差",
+                            4 => "追"
+                        });
+                        properValue += (k.running_style switch
+                        {
+                            1 => GetProper(trainedChara.proper_running_style_nige),
+                            2 => GetProper(trainedChara.proper_running_style_senko),
+                            3 => GetProper(trainedChara.proper_running_style_sashi),
+                            4 => GetProper(trainedChara.proper_running_style_oikomi)
+                        });
+                        properTypeLine.Add(properType);
+                        properValueLine.Add(properValue);
+                        speedLine.Add(trainedChara.speed.ToString());
+                        staminaLine.Add(trainedChara.stamina.ToString());
+                        powerLine.Add(trainedChara.power.ToString());
+                        gutsLine.Add(trainedChara.guts.ToString());
+                        wizLine.Add(trainedChara.wiz.ToString());
+                    }
                 }
+                table.AddRow(properTypeLine.Append("平 均").ToArray());
+                table.AddRow(properValueLine.Append("/ / /").ToArray());
+                table.AddRow(speedLine.Append(speedLine.Skip(1).Average(x => int.Parse(x)).ToString("F0")).ToArray());
+                table.AddRow(staminaLine.Append(staminaLine.Skip(1).Average(x => int.Parse(x)).ToString("F0")).ToArray());
+                table.AddRow(powerLine.Append(powerLine.Skip(1).Average(x => int.Parse(x)).ToString("F0")).ToArray());
+                table.AddRow(gutsLine.Append(gutsLine.Skip(1).Average(x => int.Parse(x)).ToString("F0")).ToArray());
+                table.AddRow(wizLine.Append(wizLine.Skip(1).Average(x => int.Parse(x)).ToString("F0")).ToArray());
+                container.AddRow(table);
             }
+            Console.BufferWidth = 160;
+            Console.SetWindowSize(Console.BufferWidth, Console.WindowHeight + 3);
+            AnsiConsole.Write(container);
 
             static string GetProper(int proper) => proper switch
             {
