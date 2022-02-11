5c3008f8ee5f9fe53e61630bbdd5ff42aef8e00e
EtherealAO <admin@nijigen.cc>
2022-02-11 13:28:45 +0800

    每次启动Windows都要用管理员权限运行一次马娘才能正常运行。为了确保安全，和DMM一样选择全程管理员权限运行

--- UmamusumeResponseAnalyzer/Server.cs
+++ UmamusumeResponseAnalyzer/Server.cs
@@ -206,7 +206,7 @@ namespace UmamusumeResponseAnalyzer
                 var (Name, WinSaddle, Score) = (name[i.card_id], i.win_saddle_id_array.Intersect(i.succession_chara_array[0].win_saddle_id_array).Count()
                         + i.win_saddle_id_array.Intersect(i.succession_chara_array[1].win_saddle_id_array).Count(), i.rank_score);
                 AnsiConsole.Write(new Rule());
-                AnsiConsole.WriteLine($"好友：{data.user_info_summary.name}\t\tFollower数：{data.follower_num}");
+                AnsiConsole.WriteLine($"好友：{data.user_info_summary.name}\t\tID：{data.user_info_summary.viewer_id}\t\tFollower数：{data.follower_num}");
                 AnsiConsole.WriteLine($"种马：{Name}\t\t{WinSaddle}\t\t{Score}");
                 AnsiConsole.Write(new Rule());
             }
