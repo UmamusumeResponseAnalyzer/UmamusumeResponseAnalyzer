290d9a2506c105fc22290e9a7c8d3b8afadd733f
EtherealAO <admin@nijigen.cc>
2022-05-14 08:52:44 +0800

    初版

---
+++ UmamusumeResponseAnalyzer/Brotli.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Compression;
+
+namespace UmamusumeResponseAnalyzer
+{
+    public static class Brotli
+    {
+        public static byte[] Compress(byte[] input)
+        {
+            using var source = new MemoryStream(input);
+            using var dest = new MemoryStream();
+            using (var brotli = new BrotliStream(dest, (CompressionLevel)11))
+                source.CopyTo(brotli);
+            return dest.ToArray();
+        }
+        public static byte[] Decompress(byte[] input)
+        {
+            using var source = new MemoryStream(input);
+            using var dest = new MemoryStream();
+            using (var brotli = new BrotliStream(source, CompressionMode.Decompress))
+                brotli.CopyTo(dest);
+            return dest.ToArray();
+        }
+    }
+}
