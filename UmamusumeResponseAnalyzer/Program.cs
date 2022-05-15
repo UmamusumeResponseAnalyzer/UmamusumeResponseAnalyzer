304dedd62506b07135332e86be310dd653deea29
tynakuh <>
2022-05-15 16:08:43 +0900

    Removed extra UmamusumeResponseAnalyzer from download URL from github url

--- UmamusumeResponseAnalyzer/Program.cs
+++ UmamusumeResponseAnalyzer/Program.cs
@@ -308,7 +308,7 @@ namespace UmamusumeResponseAnalyzer
         }
         static string GetDownloadUrl(string filepath)
         {
-            const string CNHost = "https://assets.shuise.net";
+            const string CNHost = "https://assets.shuise.net/UmamusumeResponseAnalyzer";
             const string GithubHost = "https://raw.githubusercontent.com/EtherealAO/UmamusumeResponseAnalyzer/master";
             var isCN = RegionInfo.CurrentRegion.Name == "CN" || CultureInfo.CurrentUICulture.Name == "zh-CN";
             var ext = Path.GetExtension(filepath);
@@ -317,9 +317,9 @@ namespace UmamusumeResponseAnalyzer
             var host = isCN ? CNHost : GithubHost;
             return ext switch
             {
-                ".json" => $"{host}/UmamusumeResponseAnalyzer/{filename}",
-                ".exe" => isCN ? $"{host}/UmamusumeResponseAnalyzer/{filename}" : $"https://github.com/EtherealAO/UmamusumeResponseAnalyzer/releases/latest/download/UmamusumeResponseAnalyzer.exe"
-            };
+                ".json" => $"{host}/{filename}",
+                ".exe" => isCN ? $"{host}/{filename}" : $"https://github.com/EtherealAO/UmamusumeResponseAnalyzer/releases/latest/download/UmamusumeResponseAnalyzer.exe"
+				};
         }
         static async Task DownloadAssets(ProgressContext ctx, string instruction, string path)
         {
