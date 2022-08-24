ea47587985680bd734edc7d3c041747c1240d349
EtherealAO <admin@nijigen.cc>
2022-08-24 11:41:49 +0800

    更新数据

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,13 +10,11 @@ namespace Test
         static void Main(string[] args)
         {
             Database.Initialize();
-            var bytes = File.ReadAllBytes(@"C:\Users\micro\AppData\Local\UmamusumeResponseAnalyzer\packets\22-07-26 20-52-52-663R.msgpack");
-            var obj = JsonConvert.DeserializeObject<FriendSearchResponse>(MessagePack.MessagePackSerializer.ConvertToJson(bytes));
+            var bytes = File.ReadAllBytes(@"C:\Users\micro\AppData\Local\UmamusumeResponseAnalyzer\packets\22-08-24 11-19-41-293R.msgpack");
+            var obj = JsonConvert.DeserializeObject<SingleModeCheckEventResponse>(MessagePack.MessagePackSerializer.ConvertToJson(bytes));
             if (obj == null) throw new Exception("反序列化失败");
-            //obj.AsParseSingleModeCheckEventResponseTest()
-                //.Run();
-            Console.WriteLine(JsonConvert.SerializeObject(obj, Formatting.Indented));
-            File.WriteAllText(@"C:\Users\micro\AppData\Local\UmamusumeResponseAnalyzer\packets\22-07-26 20-52-52-663R.json", JsonConvert.SerializeObject(obj, Formatting.Indented));
+            obj.AsParseCommandInfoResponseTest()
+                .Run();
         }
     }
 }
