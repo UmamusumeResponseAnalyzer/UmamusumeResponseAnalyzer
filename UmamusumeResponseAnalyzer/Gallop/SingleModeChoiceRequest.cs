621efff1c20cbbfa620249030af01a9825da27c4
xulai1001 <xulai1001@users.noreply.github.com>
2024-02-13 05:16:45 +0800

    增加事件结果跟踪，以及赌狗事件的跟踪 (#88)
    * Dec 20 update
    
    * Jan 19 update data
    
    * Jan 24 add SuccessEvent tracker
    
    * Feb 1 update data
    
    * Fix talent_skill_Sets

---
+++ UmamusumeResponseAnalyzer/Gallop/SingleModeChoiceRequest.cs
@@ -0,0 +1,25 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallop
+{
+    [MessagePackObject]
+    public class SingleModeChoiceRequest
+    {
+        [Key("event_id")]
+        public int event_id;
+        
+        [Key("chara_id")]
+        public int chara_id;
+
+        [Key("choice_number")]
+        public int choice_number;
+
+        [Key("current_turn")]
+        public int current_turn;
+    }
+}
