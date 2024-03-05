34b75cc195fc8aa4d561d77460c81038eb1250d1
离披 <lipi@shuise.dev>
2024-03-05 14:41:37 +0800

    Revert "更新了统计功能"
    This reverts commit 31c158f91de16c2521c293cee90098143b48bde4.

--- UmamusumeResponseAnalyzer/Server.cs
+++ UmamusumeResponseAnalyzer/Server.cs
@@ -168,10 +168,7 @@ namespace UmamusumeResponseAnalyzer
                         if (Config.Get(Localization.Config.I18N_ShowCommandInfo))
                         {
                             if (data.chara_info.scenario_id == 7)
-                            {
-                                Handlers.GameLogger(dyn.ToObject<Gallop.SingleModeCheckEventResponse>());
                                 Handlers.ParseSportCommandInfo(dyn.ToObject<Gallop.SingleModeCheckEventResponse>());
-                            }
                             else
                                 Handlers.ParseCommandInfo(dyn.ToObject<Gallop.SingleModeCheckEventResponse>());
                         }
